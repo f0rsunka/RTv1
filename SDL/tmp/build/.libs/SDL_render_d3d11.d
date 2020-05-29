02b7b0fad37becb0fc52db74441e972a368dc0d4
f0rsunka <f0rsunkai9@gmail.com>
2020-05-29 23:07:15 +0300

    invalide cone

--- SDL/tmp/build/.libs/SDL_render_d3d11.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_d3d11.lo: \
- /home/f0rsunka/Documents/school21/rtv1/SDL/src/render/direct3d11/SDL_render_d3d11.c \
- /home/f0rsunka/Documents/school21/rtv1/SDL/src/render/direct3d11/../../SDL_internal.h \
- /home/f0rsunka/Documents/school21/rtv1/SDL/src/render/direct3d11/../../dynapi/SDL_dynapi.h \
- /home/f0rsunka/Documents/school21/rtv1/SDL/src/render/direct3d11/../../dynapi/SDL_dynapi_overrides.h \
- include/SDL_config.h \
- /home/f0rsunka/Documents/school21/rtv1/SDL/include/SDL_platform.h \
- /home/f0rsunka/Documents/school21/rtv1/SDL/include/begin_code.h \
- /home/f0rsunka/Documents/school21/rtv1/SDL/include/close_code.h
