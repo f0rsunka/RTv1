ee83ce700a8ae59a97d341b83ddbc8c8b214fd79
dmorrige <berries.whale@gmail.com>
2020-06-09 17:04:33 +0300

    proper cylinder

--- srcs/init_scene.c
+++ srcs/init_scene.c
@@ -290,6 +290,89 @@ t_scene		*create_scene_cone(int fd, char **line)
 	return (0);
 }
 
+t_cylinder	*create_cylinder(int fd, char **line)
+{
+	t_cylinder	*cylinder;
+	int			status;
+	short		bitmask;
+	size_t		i;
+
+	bitmask = 0;
+	cylinder = (t_cylinder *)malloc(sizeof(t_cylinder));
+	ft_memdel((void**)line);
+	i = 0;
+	while ((status = get_next_line(fd, line)) > 0) {
+		if (!**line) {
+			ft_memdel((void**)line);
+			continue;
+		}
+		if (ft_strncmp(*line, "    ", 4))
+			break;
+		if (status == -1)
+		{
+			ft_putendl_fd("Read error!", 2);
+			exit(1);
+		}
+		if (read_keyed_double(*line, "    offset_x:", &(cylinder->offset.x)))
+			bitmask += 1u << 0u;
+		if (read_keyed_double(*line, "    offset_y:", &(cylinder->offset.y)))
+			bitmask += 1u << 1u;
+		if (read_keyed_double(*line, "    offset_z:", &(cylinder->offset.z)))
+			bitmask += 1u << 2u;
+		if (read_keyed_double(*line, "    coef_x:", &(cylinder->coef.x)))
+			bitmask += 1u << 3u;
+		if (read_keyed_double(*line, "    coef_y:", &(cylinder->coef.y)))
+			bitmask += 1u << 4u;
+		if (read_keyed_double(*line, "    coef_z:", &(cylinder->coef.z)))
+			bitmask += 1u << 5u;
+		if (read_keyed_double(*line, "    angle_x:", &(cylinder->angle.x)))
+			bitmask += 1u << 6u;
+		if (read_keyed_double(*line, "    angle_y:", &(cylinder->angle.y)))
+			bitmask += 1u << 7u;
+		if (read_keyed_double(*line, "    angle_z:", &(cylinder->angle.z)))
+			bitmask += 1u << 8u;
+		if (read_keyed_float(*line, "    color_r:", &(cylinder->material.color.r)))
+			bitmask += 1u << 9u;
+		if (read_keyed_float(*line, "    color_g:", &(cylinder->material.color.g)))
+			bitmask += 1u << 10u;
+		if (read_keyed_float(*line, "    color_b:", &(cylinder->material.color.b)))
+			bitmask += 1u << 11u;
+		if (read_keyed_float(*line, "    specular:", &(cylinder->material.specular)))
+			bitmask += 1u << 12u;
+		if (read_keyed_float(*line, "    radius:", &(cylinder->radius))) {
+			bitmask += 1u << 13u;
+			cylinder->radius /= 100;
+		}
+		i++;
+	}
+	if (bitmask != ((1u << 14u) - 1) || i != 14)
+	{
+		ft_memdel((void **)&cylinder);
+		ft_putendl_fd("Invalid struct!", 2);
+		exit(1);
+	}
+	cylinder->material.color = float_to_byte(cylinder->material.color);
+	return (cylinder);
+}
+
+t_scene		*create_scene_cylinder(int fd, char **line)
+{
+	char	*str;
+	t_scene *tmp;
+
+	str = ft_strstr(*line, "  cylinder:");
+	if (*line == str && str != 0)
+	{
+		tmp = (t_scene *)malloc(sizeof(t_scene));
+		tmp->object = (void *)create_cylinder(fd, line);
+		tmp->type = TYPE_CYLINDER;
+		tmp->next = 0;
+
+		return (tmp);
+	}
+	return (0);
+}
+
 void		parse_light(int fd, char **line, t_light *light)
 {
 	int		status;
@@ -384,8 +467,8 @@ int			create_figure(int fd, t_scene *prev, char **line)
 		return (1);
 	if ((prev->next = create_scene_cone(fd, line)))
 		return (1);
-//	if ((prev->next = create_scene_cylinder(fd, line)))
-//		return (1);
+	if ((prev->next = create_scene_cylinder(fd, line)))
+		return (1);
 	return (0);
 }
 
